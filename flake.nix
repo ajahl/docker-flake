53a1b4ccb0715ed1793a9c4a0cb39c1be76c7641
Alex Jahl <ajahl.se@gmail.com>
2025-01-08 07:42:44 +0100

    inital

---
+++ flake.nix
@@ -0,0 +1,37 @@
+{
+  description = "Docker Environment flake";
+
+  inputs.nixpkgs.url = "github:NixOS/nixpkgs";
+
+  outputs = { self, nixpkgs }:
+    let
+      pkgs = import nixpkgs {
+        system = "x86_64-darwin";
+      };
+
+    in {
+      devShell.x86_64-darwin = pkgs.mkShell {
+        buildInputs = [
+          pkgs.colima
+          pkgs.docker
+          pkgs.lazydocker
+        ];
+
+        shellHook = ''
+          echo "Setting up docker environment"
+          colima start
+          docker --version
+          # lazydocker
+
+          # Function to stop Colima
+          stop_colima() {
+            echo "Stopping Colima..."
+            colima stop
+          }
+
+          # Set up to stop Colima on shell exit
+          trap stop_colima EXIT
+          '';
+        };
+      };
+  }
